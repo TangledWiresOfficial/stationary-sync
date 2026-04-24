c7a89173d8271868cc07869de0bd71f74aca3181
xWires <xwires@tangledwires.xyz>
2026-04-24 17:27:22 +0100

    Fix json field names and remove extra fields from json

--- app/controllers/api/v1/journeys_controller.rb
+++ app/controllers/api/v1/journeys_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::JourneysController < ApplicationController
   def index
-    render json: @user.journeys
+    render json: @user.journeys.as_json(
+      include: [ parts: { only: [ :line, :station ] } ],
+      only: [ :timestamp, :uuid ]
+    )
   end
 end
--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,10 +1,10 @@
 class Journey < ApplicationRecord
   belongs_to :user
 
-  has_many :journey_parts, dependent: :destroy
-  validates :journey_parts, length: { minimum: 1 }
+  has_many :parts, class_name: "JourneyPart", dependent: :destroy
+  validates :parts, length: { minimum: 1 }
 
-  accepts_nested_attributes_for :journey_parts
+  accepts_nested_attributes_for :parts
 
   validates :timestamp, presence: true
   validates :uuid, presence: true, uniqueness: true
