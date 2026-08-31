9307f780c73e6c19f19a0431945bddf195cb8262
xWires <xwires@tangledwires.xyz>
2026-08-31 17:25:50 +0100

    Remove name and email from Users
    We only really need the uid to figure out who it is so there's no point storing more data than we absolutely have to

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_24_100832) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_31_162424) do
   create_table "journey_parts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.integer "journey_id"
@@ -31,8 +31,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_24_100832) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.string "email"
-    t.string "name"
     t.string "uid"
     t.datetime "updated_at", null: false
   end
